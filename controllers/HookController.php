b9043522bd23a0d6c33307a5f8a21d5d470b6a59
mrstarmech <mr.starmech@gmail.com>
2022-04-15 17:41:27 +0700

    additional check param

--- controllers/HookController.php
+++ controllers/HookController.php
@@ -9,7 +9,7 @@ class HookController extends Controller
 {
     public function actionTb() {
         $rq_params = Yii::$app->request->queryParams;
-        if (isset($rq_params['tbl_click_id'])) {
+        if (isset($rq_params['tbl_click_id']) && isset($rq_params['tbl_event_name'])) {
             $ch = curl_init("https://trc.taboola.com/actions-handler/log/3/s2s-action?click-id=" . $rq_params['tbl_click_id'] . "&name=" . $rq_params['tbl_event_name']);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $tbl_resp = curl_exec($ch);
