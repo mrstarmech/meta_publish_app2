037c8b553b9ba146dd49f19e242a2aafb5718f7a
mrstarmech <mr.starmech@gmail.com>
2022-04-15 17:16:26 +0700

    get para

--- controllers/HookController.php
+++ controllers/HookController.php
@@ -8,7 +8,7 @@ use Yii;
 class HookController extends Controller
 {
     public function actionTb() {
-        $rq_params = Yii::$app->requestedParams;
+        $rq_params = Yii::$app->request->params;
         if (isset($rq_params['tbl_click_id'])) {
             return "https://trc/?click-id=" . $rq_params['tbl_click_id'] . "&name=event_name";
         }
