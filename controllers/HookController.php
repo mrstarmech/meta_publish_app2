978e191284c57d016d15d3f937b28fb08fac8218
mrstarmech <mr.starmech@gmail.com>
2022-04-16 18:48:12 +0700

    post hook

--- controllers/HookController.php
+++ controllers/HookController.php
@@ -25,14 +25,24 @@ class HookController extends Controller
     public function actionTbp() {
         $rq_params = Yii::$app->request->queryParams;
         if (isset($rq_params['tbl_click_id']) && isset($rq_params['tbl_event_name'])) {
-            $url = "https://trc.taboola.com/actions-handler/log/3/s2s-action?click-id=" . $rq_params['tbl_click_id'] . "&name=" . $rq_params['tbl_event_name'];
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
+            $content = json_encode([
+                "actions" => [
+                    [
+                        "click-id" => $rq_params['tbl_click_id'],
+                        "timestamp" => time(),
+                        "name" => "make_purchase"
+                    ]
+                ]
+            ]);
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, 'https://trc.taboola.com/1388223/log/3/bulk-s2s-action');
             curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json", "Content-length: " . strlen($content)));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $tbl_resp = curl_exec($ch);
             
             curl_close($ch);
-            return var_dump(["Response" => $tbl_resp, "Url" => $url]);
+            return var_dump($tbl_resp, $content);
         }
         return 'ne OK';
     }
