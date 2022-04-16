e9d8a4e216d4672775caa3a4d05c55076f25d325
mrstarmech <mr.starmech@gmail.com>
2022-04-16 18:12:26 +0700

    upd hook

--- controllers/HookController.php
+++ controllers/HookController.php
@@ -12,8 +12,10 @@ class HookController extends Controller
         if (isset($rq_params['tbl_click_id']) && isset($rq_params['tbl_event_name'])) {
             $url = "https://trc.taboola.com/actions-handler/log/3/s2s-action?click-id=" . $rq_params['tbl_click_id'] . "&name=" . $rq_params['tbl_event_name'];
             $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $tbl_resp = curl_exec($ch);
+            
             curl_close($ch);
             return var_dump(["Response" => $tbl_resp, "Url" => $url]);
         }
