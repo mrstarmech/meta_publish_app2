df8b506ddce01f318af385b44d20382ca9ff2197
mrstarmech <mr.starmech@gmail.com>
2022-04-16 19:23:36 +0700

    test

--- controllers/HookController.php
+++ controllers/HookController.php
@@ -8,18 +8,7 @@ use Yii;
 class HookController extends Controller
 {
     public function actionTbp() {
-        $rq_params = Yii::$app->request->queryParams;
-        if (isset($rq_params['tbl_click_id']) && isset($rq_params['tbl_event_name'])) {
-            $url = "https://trc.taboola.com/actions-handler/log/3/s2s-action?click-id=" . $rq_params['tbl_click_id'] . "&name=" . $rq_params['tbl_event_name'];
-            $ch = curl_init($url);
-            curl_setopt($ch, CURLOPT_PROTOCOLS, CURLPROTO_HTTPS);
-            curl_setopt($ch, CURLOPT_POST, true);
-            $tbl_resp = curl_exec($ch);
-            
-            curl_close($ch);
-            return var_dump(["Response" => $tbl_resp, "Url" => $url]);
-        }
-        return 'ne OK';
+        return var_dump(substr(microtime(true) * 1000, 0, 13));
     }
 
     public function actionTb() {
